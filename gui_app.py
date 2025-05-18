97db5a54363720dd5d4b9d10b1b06f80a6666cb1
krovskiy <168024863+krovskiy@users.noreply.github.com>
2025-05-18 20:56:34 +0300

    first steps in adding gui

--- gui_app.py
+++ gui_app.py
@@ -1,13 +1,39 @@
+import tkinter.filedialog
 import tkinter.font
 import tkinter.ttk
+import threading
 from tkinter import *
 from tkinter import ttk
 from main import play, filename,stop_playback
 
 
+
 root = tkinter.Tk()
-frm = ttk.Frame(root,padding=10)
-frm.grid()
+title = root.title("midi-to-appeal")
+textInsert = tkinter.Text(root, height=2)
+textInsert.pack()
+
+
+
+def open_file():
+    filedialog = tkinter.filedialog.askopenfilename(initialdir='/', title="Select file", filetypes = (("MIDI files", "*.mid"),))
+    textInsert.delete("1.0", tkinter.END)
+    textInsert.insert('1.0', str(filename))
+
+
+
+
+top = Frame(root)
+top.pack()
+bottom = Frame(root)
+bottom.pack(side=BOTTOM)
+bottom2 = Frame(root)
+bottom2.pack(side=BOTTOM)
+
+button1 = Button(top, text="hii faggot!",fg="red")
+button1.pack()
+button2 = Button(bottom2, text="open a file", fg="red", command= open_file)
+button2.pack()
 
 
 root.mainloop()
